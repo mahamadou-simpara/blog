6aab370021055c8d0fce969d4c283fd1f881e234
mahamadou-simpara <msimpara@doninya.com>
2023-05-23 17:18:27 +0000

    database set up done !

--- data/database.js
+++ data/database.js
@@ -1,1 +1,10 @@
-require('mysql2')
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    database: 'blog',
+    user: 'root',
+    password: 'mysqltest'
+})
+
+module.exports = pool;
