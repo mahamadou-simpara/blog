c86ccae7e2072fb1d53e1b47cb7591b36921eebf
mahamadou-simpara <msimpara@doninya.com>
2023-05-24 11:05:24 +0000

    Fetching part done !

--- routes/blog.js
+++ routes/blog.js
@@ -3,12 +3,13 @@ const db = require('../data/database');
 const express = require('express');
 
 const router = express.Router();
+
 router.get('/', function(req, res){
     res.redirect('/posts');
 });
 
 router.get('/posts', async function(req, res){
-    const [posts] =  await   db.query('SELECT * FROM posts ');
+    const [posts] =  await   db.query('SELECT posts.*, authors.name FROM posts INNER JOIN authors on posts.author_id = authors.id');
     res.render('posts-list', { posts: posts});
 });
 
@@ -18,7 +19,7 @@ router.get('/new-post', async function(req, res){
     res.render('create-post', {authors: authors});
 });
 
-router.post('/posts', function(req, res){
+router.post('/posts', async function(req, res){
     const data = [
     req.body.title,
     req.body.summary,
@@ -26,9 +27,27 @@ router.post('/posts', function(req, res){
     req.body.author
     ];
     
-    db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [data]);
+    await db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [data]);
 
     res.redirect('/posts');
+})
+
+router.get('/posts/:id', async function(req, res){
+    const postID = req.params.id;
+
+    
+    const [posts] =  await   db.query('SELECT * FROM posts');
+
+    if(postID === [posts[0]])
+
+    for (const post of posts){
+     if(postID === post.id){
+        res.render('post-detail', {id: postID})
+     }
+    }
+ 
+    res.render('404');
+    
 })
 
 module.exports = router;
