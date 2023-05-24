0b0c92caf0068668a43f5f87960d10cffc953869
mahamadou-simpara <msimpara@doninya.com>
2023-05-24 00:38:08 +0000

    Start with displaying data !

--- routes/blog.js
+++ routes/blog.js
@@ -1,3 +1,5 @@
+const db = require('../data/database');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,12 +7,28 @@ router.get('/', function(req, res){
     res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res){
-    res.render('posts-list');
+router.get('/posts', async function(req, res){
+    const [posts] =  await   db.query('SELECT * FROM posts ');
+    res.render('posts-list', { posts: posts});
 });
 
-router.get('/new-post', function(req, res){
-    res.render('create-post');
+
+router.get('/new-post', async function(req, res){
+    const [authors] = await db.query('SELECT * FROM authors');
+    res.render('create-post', {authors: authors});
 });
+
+router.post('/posts', function(req, res){
+    const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author
+    ];
+    
+    db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [data]);
+
+    res.redirect('/posts');
+})
 
 module.exports = router;
